4c7b7ec0705e6fd5f5d1e61c44332ca8c0b24d3e
James Craig <jamescraig@finervision.com>
2020-01-14 11:14:04 +0000

    Updated prod script

--- bin/updateProd.sh
+++ bin/updateProd.sh
@@ -4,7 +4,7 @@ git pull
 
 docker system prune -f
 
-docker-compose -f docker-compose.prod.yml up --build
+docker-compose -f docker-compose.prod.yml up --build -d
 
 docker-compose -f docker-compose.prod.yml exec api php artisan migrate
 
